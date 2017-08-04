b17fdbb922b7b1704dbae123f50e678ddebe9353
Artem Verkhoglyadov <artem@aimtech.com>
2017-08-04 21:34:27 +0300

    Make real random solution

--- solutions/random_solution.cpp
+++ solutions/random_solution.cpp
@@ -1,8 +1,14 @@
 #include "launcher.h"
+#include <algorithm>
 
 River make_move_random(const Map& map) {
-  for (auto& edges : map.graph) {
-    for (auto& e : edges) {
+  std::vector<Vertex> vertices(map.graph.size());
+  for (size_t i = 0; i < vertices.size(); ++i) {
+    vertices[i] = i;
+  }
+  std::random_shuffle(vertices.begin(), vertices.end());
+  for (auto v : vertices) {
+    for (auto& e : map.graph[v]) {
       if (e.owner == kNoOwner) {
         return map.get_river(e);
       }
