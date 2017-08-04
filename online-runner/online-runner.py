1db70d7a1e4e6a03551783212ded3f04e7417457
Ivan Fefer <fefer.ivan@gmail.com>
2017-08-04 19:34:39 +0300

    fix online runner

--- online-runner/online-runner.py
+++ online-runner/online-runner.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import socket
 import subprocess
+import sys
 
 BUFFER_SIZE = 4*1024
 
@@ -55,15 +56,16 @@ def receive_json(sock):
   return json.loads(data.decode("utf-8"))
 
 
-def receive_json_from_strategy(strategy):
+def receive_json_from_strategy(stdout_data):
   expected_length = 0
-  while True:
-    b = strategy.stdout.read(1)[0]
+  prefix_length = 0
+  for b in stdout_data:
+    prefix_length += 1
     if b == ord(':'):
       break
     else:
       expected_length = 10 * expected_length + b - ord('0')
-  data = strategy.stdout.read(expected_length)
+  data = stdout_data[prefix_length:expected_length+prefix_length]
   return json.loads(data.decode('utf-8'))
 
 
@@ -71,42 +73,51 @@ def online_handshake(sock, name):
   send_json(sock, {"me": name})
   answer = receive_json(sock)
   assert answer["you"] == name
+  print('successful handshake with server name: ' + name, file=sys.stderr)
 
+def run_subprocess(binary):
+  return subprocess.Popen([binary], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+SERVER = "punter.inf.ed.ac.uk"
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("server")
   parser.add_argument("port")
   parser.add_argument("name")
   parser.add_argument("binary")
   args = parser.parse_args()
 
   sock = socket.socket()
-  sock.connect((args.server, int(args.port)))
+  sock.connect((SERVER, int(args.port)))
 
   online_handshake(sock, args.name)
 
-  state = {}
-  with subprocess.Popen([args.binary], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as strategy:
-    setup_json = receive_json(sock)
-    our_id = setup_json["punter"]
-    send_json_to_strategy(strategy, setup_json)
-    ready_json = receive_json_from_strategy(strategy)
-    state = ready_json["state"]
-    assert ready_json["ready"] == our_id
-    send_json(sock, {"ready": our_id})
-
-
+  setup_json = receive_json(sock)
+  print(
+      'received setup json our_id: {} total players: {}'.format(
+          setup_json["punter"], setup_json["punters"]),
+      file=sys.stderr)
+  our_id = setup_json["punter"]
+  setup_stdout, setup_stderr = run_subprocess(args.binary).communicate(json_to_bytearray(setup_json))
+  print('strategy stderr: ' + setup_stderr.decode('utf-8'), file=sys.stderr)
+  ready_json = receive_json_from_strategy(setup_stdout)
+  assert ready_json["ready"] == our_id
+  state = ready_json["state"]
+  print('strategy state size:', len(json.dumps(state)), file=sys.stderr)
+  send_json(sock, {"ready": our_id})
+  
   while True:
     moves_json = receive_json(sock)
     moves_json["state"] = state
-    with subprocess.Popen([args.binary], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as strategy:
-      send_json_to_strategy(strategy, moves_json)
-      if "stop" in move_json:
-        break
-      new_move_json = receive_json_from_strategy(strategy)
-      state = new_move_json.pop("state")
-      send_json(sock, new_move_json)
+    print('received moves', file=sys.stderr)
+    new_move_stdout, new_move_stderr = run_subprocess(args.binary).communicate(json_to_bytearray(moves_json))
+    print('strategy stderr: ' + new_move_stdout.decode('utf-8'), file=sys.stderr)
+    if "stop" in moves_json:
+      break
+    new_move_json = receive_json_from_strategy(new_move_stdout)
+    state = new_move_json.pop("state")
+    print('strategy state size:', len(json.dumps(state)), file=sys.stderr)
+    send_json(sock, new_move_json)
 
 
 if __name__ == "__main__":
