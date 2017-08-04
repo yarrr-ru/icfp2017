a73d1071018a372c0e2e5d3a275c13a11879e320
Artem Verkhoglyadov <artem@aimtech.com>
2017-08-05 01:33:41 +0300

    Add get_worst_edge

--- solutions/greed.h
+++ solutions/greed.h
@@ -8,5 +8,7 @@ std::pair<int64_t, std::vector<Edge>> get_path(
     const std::vector<Punter>& river_owners
 );
 
+Edge get_worst_edge(Vertex from, Vertex to, const Map& map);
+
 River make_move_greed_only_st(const Map& map);
 River make_move_greed(const Map& map);
